fc2bed82f7c458f57f941982bc721f281eec139f
Mahindra Kumar <129785828+Mahindra8252@users.noreply.github.com>
2025-10-01 01:22:38 +0530

    Update insertion.js

--- Sorting/insertion.js
+++ Sorting/insertion.js
@@ -2,8 +2,6 @@ var beep = new Audio('beep3.mp3')
 var mouseclick = new Audio('Mouseclick.mp3')
 var done = new Audio('wrong.mp3')
 
-
-
 const InsertionSortButton = document.querySelector(".InsertionSort");
 InsertionSortButton.addEventListener('click', async function () {
     // headingchange1.textContent = "Insertion Sort";
@@ -20,8 +18,8 @@ InsertionSortButton.addEventListener('click', async function () {
     disableSizeSlider();
     disableNewArrayBtn();
     await InsertionSort();
-    // enableSortingBtn();
-    // enableSizeSlider();
+    enableSortingBtn();
+    enableSizeSlider();
     enableNewArrayBtn();
 });
 
@@ -37,54 +35,25 @@ async function descriptionText_insertion() {
     const code = document.querySelector('#code_java')
     // console.log(code.innerHTML)
     code.innerHTML = `// C++ program for insertion sort
-#include <bits/stdc++.h>
-using namespace std;
 
-// Function to sort an array
-// using insertion sort
 void insertionSort(int arr[], int n)
 {
-	int i, key, j;
-	for (i = 1; i < n; i++) {
-		key = arr[i];
-		j = i - 1;
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
 
-		// Move elements of arr[0..i-1],
-		// that are greater than key, to one
-		// position ahead of their
-		// current position
-		while (j >= 0 && arr[j] > key) {
-			arr[j + 1] = arr[j];
-			j = j - 1;
-		}
-		arr[j + 1] = key;
-	}
+        // Move elements of arr[0..i-1],
+        // that are greater than key, to one
+        // position ahead of their
+        // current position
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
 }
-
-// A utility function to print
-// an array of size n
-void printArray(int arr[], int n)
-{
-	int i;
-	for (i = 0; i < n; i++)
-		cout << arr[i] << " ";
-	cout << endl;
-}
-
-// Driver code
-int main()
-{
-	int arr[] = { 12, 11, 13, 5, 6 };
-	int N = sizeof(arr) / sizeof(arr[0]);
-
-	insertionSort(arr, N);
-	printArray(arr, N);
-
-	return 0;
-}
-// This is code is contributed by rathbhupendra
-
-
 
 
 `
@@ -112,12 +81,16 @@ async function InsertionSort() {
     for (let i = 1; i < element.length; i++) {
         let j = i - 1;
         let p = element[i].style.height;
+         let val = element[i].innerHTML; // store value text
         element[i].style.background = 'rgb(250, 5, 54)';
         await waitforme(delay);
 
         while (j >= 0 && (parseInt(element[j].style.height) > parseInt(p))) {
             element[j].style.background = 'rgb(9, 102, 2)';
+
             element[j + 1].style.height = element[j].style.height;
+            element[j + 1].innerHTML = element[j].innerHTML;  // <-- update text value
+
             j--;
             beep.play();
             await waitforme(delay);
@@ -128,6 +101,7 @@ async function InsertionSort() {
             }
         }
         element[j + 1].style.height = p;
+        element[j + 1].innerHTML = parseInt(p) / 4; // since height = value * 4
         element[i].style.background = 'rgb(3, 252, 11)';
     }
     selectText.innerHTML=`Sorting Complete!`
